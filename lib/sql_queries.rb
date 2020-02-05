11b51afc2b924cd9229191bba31872007ff7fa72
alejandra cantu <alejandra.cantu13@gmail.com>
2020-02-05 17:12:12 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,7 +28,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT MAX(color), COUNT(MAX(color))
+  "SELECT MAX(color) as prominent_color, COUNT(prominent_color)
   FROM bears;
 "
 end
