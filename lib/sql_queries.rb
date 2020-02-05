24ed7d7ae2dc38bc763e30f54126ec3aea4a356d
alejandra cantu <alejandra.cantu13@gmail.com>
2020-02-05 17:17:38 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,14 +28,15 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT MAX(color) as prominent_color, COUNT(prominent_color)
+  "SELECT MAX(color), 
   FROM bears;
 "
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT
-  FROM"
+  "SELECT COUNT(bears)
+  FROM bears
+  WHERE temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
